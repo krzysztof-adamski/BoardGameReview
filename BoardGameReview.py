cb3ba03202f6e1b44fc75bb2cf88c6955b268ba1
Prince Mishra <mishraprince721@gmail.com>
2020-04-08 13:17:49 +0530

    changes

--- BoardGameReview.py
+++ BoardGameReview.py
@@ -1,9 +1,8 @@
-
+#Board Game review using linear regression
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 
 df=pd.read_csv("https://raw.githubusercontent.com/Maduflavins/Board-Game-Review/master/games.csv")
 df.columns
